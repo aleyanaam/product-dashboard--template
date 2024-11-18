d72a531889f9d2fc67b52f0c3d4fe2430f78b3f0
Aleyana McLeod <aleyanam@usf.edu>
2024-11-17 22:00:28 -0500

    Implement keys for each ProductItem in ProductList

--- src/Productlist.jsx
+++ src/Productlist.jsx
@@ -2,13 +2,19 @@
 import React from "react";
 import ProductItem from "./ProductItem";
 
+
+//Task 5 implementing keys
 const ProductList = ({ products }) => {
   return (
     <div>
       <h2>Product List</h2>
+      <ul>
         {products.map((product) => (
+            <li key={product.id} >
             <ProductItem product={product} />
+            </li>
         ))}
+        </ul>
     </div>
   );
 };
